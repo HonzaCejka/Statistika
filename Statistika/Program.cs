c1a49eef4332af3374012b7a154143fb6e1da7d6
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2021-11-23 09:40:24 +0100

    nums

--- Statistika/Program.cs
+++ Statistika/Program.cs
@@ -9,7 +9,7 @@ namespace Statistika
             ConsoleKeyInfo key;
             string nums = "0123456789";
             string text = "";
-            
+            int rowNum = 0;
             do
             {
                 
@@ -20,25 +20,27 @@ namespace Statistika
                     Console.WriteLine();
                     Console.WriteLine(text);
                     text = "";
+                    rowNum += 3;
                 }
                 else if (key.Key == ConsoleKey.Backspace && text.Length > 0)
                 {
-                    Console.SetCursorPosition(0, 0);
+                    Console.SetCursorPosition(0, rowNum);
                     Console.Write(new string(' ', text.Length));
                     text = text.Substring(0,text.Length - 1);
                     
-                    Console.SetCursorPosition(0, 0);
+                    Console.SetCursorPosition(0, rowNum);
                     Console.Write(text);
                 
                 }
                 else if (key.KeyChar == '0') 
                 {
                     Console.Write(key.KeyChar);
+                    
                 }
                 else if (nums.Contains(key.KeyChar))
                 {
                     text = text + key.KeyChar;
-                    Console.SetCursorPosition(0, 0);
+                    Console.SetCursorPosition(0, rowNum);
                     Console.Write(text);
                 }
                 

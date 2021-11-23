201b9951e7054cc7156da7012f4c2969cd046bf0
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2021-11-23 09:35:44 +0100

    backspace

--- Statistika/Program.cs
+++ Statistika/Program.cs
@@ -6,7 +6,47 @@ namespace Statistika
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConsoleKeyInfo key;
+            string nums = "0123456789";
+            string text = "";
+            
+            do
+            {
+                
+                key = Console.ReadKey(true);
+                
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(text);
+                    text = "";
+                }
+                else if (key.Key == ConsoleKey.Backspace && text.Length > 0)
+                {
+                    Console.SetCursorPosition(0, 0);
+                    Console.Write(new string(' ', text.Length));
+                    text = text.Substring(0,text.Length - 1);
+                    
+                    Console.SetCursorPosition(0, 0);
+                    Console.Write(text);
+                
+                }
+                else if (key.KeyChar == '0') 
+                {
+                    Console.Write(key.KeyChar);
+                }
+                else if (nums.Contains(key.KeyChar))
+                {
+                    text = text + key.KeyChar;
+                    Console.SetCursorPosition(0, 0);
+                    Console.Write(text);
+                }
+                
+            } while (key.Key != ConsoleKey.Escape);
+
+
+
+
         }
     }
 }
